60ef189b2a20286639941cb8aeb0255076dceb70
Alexander Kondra and Nathan Wangemann <alexander@>
2019-06-04 13:28:02 -0700

    add flashcard content, add jquery function to toggle definitions showing, add background color, change text size

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,16 @@ $(document).ready(function() {
   $("#JS").click(function() {
     $("#JShow").toggle();
   });
+  $("#variableName").click(function() {
+    $("#variableNameShow").toggle();
+  });
+  $("#methods").click(function() {
+    $("#methodsShow").toggle();
+  });
+  $("#arguments").click(function() {
+    $("#argumentsShow").toggle();
+  });
+  $("#parameters").click(function() {
+    $("#parametersShow").toggle();
+  });
 });
